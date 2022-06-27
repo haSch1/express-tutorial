91130d15e39247fbf227b943115535f4bafe424c
haSch1 <github@harry-schmid.de>
2022-06-27 16:58:42 +0200

    alle unsinnie nconsole.logs raus

--- server.js
+++ server.js
@@ -3,25 +3,21 @@ const app = express();
 
 //read
 app.get('*', (req, res) => {
-    console.log(req);
     res.send('<h1> hallo GET express</h1>')  
 });
 
 //create
 app.post('*', (req, res) => {
-    console.log(req);
     res.send('<h1> hallo POST express</h1>')  
 });
 
 //update
 app.put('*', (req, res) => {
-    console.log(req);
     res.send('<h1> hallo PUT express</h1>') 
 });
 
 //delete
 app.delete('*', (req, res) => {
-    console.log(req);
     res.send('<h1> hallo DELETE express</h1>')
 });
 
